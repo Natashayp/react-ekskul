41ac07d4f6b88c29b19d1e4a2b023157da462704
Natashayp <natashayosefani@gmail.com>
2022-02-11 17:43:49 +0700

    menambahkan file react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Info from "./components/Info";
+import Counter from "./components/Counter";
+import "./App.css";
+import Image from "./components/Image";
+import { NodeBuilderFlags } from "typescript";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-wrapper">
+        <Info nama="Tasha" kelas="XII MIPA 3" isMorning={true} />
+        <Counter />
+        <Image />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Image.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Image extends React.Component {
+  render() {
+    return (
+      <div>
+        <img width={160} height={200} src="https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659652_960_720.png" />
+      </div>
+    );
+  }
+}
+
+export default Image;
